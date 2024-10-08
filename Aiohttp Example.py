584726f231353f078d0ccaf061740ddd822f7443
Gentrit <Gentrithoxha78@gmail.com>
2024-10-08 02:17:29 +0200

    initial committ

--- Aiohttp Example.py
+++ Aiohttp Example.py
@@ -1,13 +1,25 @@
 import asyncio
 import aiohttp
 
+# Variable to adjust the number of requests at the same time
+num_requests = 5
+
+
+async def fetch(session, url, proxy):
+    async with session.get(url, proxy=proxy) as response:
+        # print(response.status)
+        print(await response.json())
+
 
 async def main():
+    url = "http://httpbin.org/ip"
+    proxy = "http://apvx45n24g1q810:32cp09nenvb27c2@rp.proxyscrape.com:6060"
+
     async with aiohttp.ClientSession() as session:
-        async with session.get("http://httpbin.org/ip",
-                               proxy="http://@rp.proxyscrape.com:6060") as response:
-            print(response.status)
-            print(await response.json())
+        # Create a list of tasks
+        tasks = [fetch(session, url, proxy) for _ in range(num_requests)]
+        # Run all tasks concurrently
+        await asyncio.gather(*tasks)
 
 
 if __name__ == '__main__':
